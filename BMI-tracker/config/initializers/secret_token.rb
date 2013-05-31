7732dc0648bf44ba26e19ce497e3d3ec5fd8089d
Gabriel Brenner <gabejbrenner@gmail.com>
2013-05-30 23:11:02 -0500

    blah

--- BMI-tracker/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BMITracker::Application.config.secret_token = '077808fa0fc11dc5c949a5219e45219f8f49afa4a559ac08c9521a5ab59bf6812c9d6e36008d29773abc6df7658baeba33d6da567cadf628519eed9399dae687'
